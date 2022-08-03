7a8c89c6faf805ec34d4bca622e2058e0d39accf
jjavierf64 <jjavierf64@estudiantec.cr>
2022-08-03 14:46:09 -0600

    Graficación

--- data_manipulation/graph.jl
+++ data_manipulation/graph.jl
@@ -1,2 +1,18 @@
 using Printf
 using Statistics
+using CSV
+using Plots
+println("Hello Graph")
+
+
+
+datos = CSV.File("./assets/datos01.csv")
+x= []
+y= []
+
+for row in datos
+    append!(x, row[1])
+    append!(y, row[2])
+end
+
+plot(x,y)
